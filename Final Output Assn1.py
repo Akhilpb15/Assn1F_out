81bab792b2b37c8a03970018f58ca2f933d542e4
Akhilpb15 <104380444+Akhilpb15@users.noreply.github.com>
2022-04-25 23:48:10 +0530

    Add files via upload

---
+++ Final Output Assn1.py
@@ -0,0 +1,38 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:\Users\asus\OneDrive\Desktop\PP\Assignment 1.py ==========
+This program can calculate avg of three numbers
+Enter the 1st number:- 10
+Enter the 2nd number:- 20
+Enter the 3rd number:- 30
+The Average of three given numbers is:  20.0
+*-:::::::::::::::::::::::-
+PYTHON PROGRAM TO CALCULATE THE INCOME TAX OF PERSON AFTER SEVERAL DEDUCTION OF MONEY (GROSS INCOME>$25000)
+Please Enter the Gross Income:$ 1000000
+Please Enter the number of dependents: 2
+The total income tax of a person in $:  196800.0
+*
+-:::::::::::::::::::::::-
+Enter the SID: 
+21112
+Enter the name of the student: 
+XYZ
+Enter the gender of the student:
+M
+Enter your Course Name:
+PY
+Enter the CGPA of the student:
+9
+[21112, 'XYZ', 'M', 'PY', 9.0]
+-:::::::::::::::::::::::-
+This python program can be use to enter marks of 5 students
+Enter the Marks of First Student:85
+Enter the Marks of Second Student: 89
+Enter the Marks of Third Student: 94
+Enter the Marks of Fourth Student: 76
+Enter the Marks of Fifth Student: 88
+The List of marks of five student is as follows:  [85, 89, 94, 76, 88]
+The List of marks of five students in a sorted manner:  [76, 85, 88, 89, 94]
+*-::::::::::::::-*
+The modified list is color 2 = ['Red', 'Green', 'White', 'Purple', 'Yellow']
